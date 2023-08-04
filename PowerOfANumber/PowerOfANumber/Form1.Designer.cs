5cef5dd03249fefdf82c71230c7446045313b37b
Pelin BİNGÖL <126492207+pelinbingl@users.noreply.github.com>
2023-08-05 00:03:12 +0300

    Add files via upload

---
+++ PowerOfANumber/PowerOfANumber/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace PowerOfANumber
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.lblPower = new System.Windows.Forms.Label();
+            this.lblNumber = new System.Windows.Forms.Label();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.txtPower = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(102, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(245, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Power Of A Number";
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblResult.Location = new System.Drawing.Point(84, 258);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(69, 20);
+            this.lblResult.TabIndex = 1;
+            this.lblResult.Text = "Result:";
+            // 
+            // lblPower
+            // 
+            this.lblPower.AutoSize = true;
+            this.lblPower.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblPower.Location = new System.Drawing.Point(84, 179);
+            this.lblPower.Name = "lblPower";
+            this.lblPower.Size = new System.Drawing.Size(67, 20);
+            this.lblPower.TabIndex = 2;
+            this.lblPower.Text = "Power:";
+            // 
+            // lblNumber
+            // 
+            this.lblNumber.AutoSize = true;
+            this.lblNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblNumber.Location = new System.Drawing.Point(84, 106);
+            this.lblNumber.Name = "lblNumber";
+            this.lblNumber.Size = new System.Drawing.Size(80, 20);
+            this.lblNumber.TabIndex = 3;
+            this.lblNumber.Text = "Number:";
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Location = new System.Drawing.Point(182, 100);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(100, 22);
+            this.txtNumber.TabIndex = 4;
+            // 
+            // txtPower
+            // 
+            this.txtPower.Location = new System.Drawing.Point(182, 177);
+            this.txtPower.Name = "txtPower";
+            this.txtPower.Size = new System.Drawing.Size(100, 22);
+            this.txtPower.TabIndex = 5;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(182, 256);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(100, 22);
+            this.txtResult.TabIndex = 6;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnCalculate.Location = new System.Drawing.Point(214, 316);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(133, 40);
+            this.btnCalculate.TabIndex = 7;
+            this.btnCalculate.Text = "CALCULATE";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(463, 450);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtPower);
+            this.Controls.Add(this.txtNumber);
+            this.Controls.Add(this.lblNumber);
+            this.Controls.Add(this.lblPower);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label lblPower;
+        private System.Windows.Forms.Label lblNumber;
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.TextBox txtPower;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Button btnCalculate;
+    }
+}
+
---
+++ PowerOfANumber/PowerOfANumber/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PowerOfANumber
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            string numberText = txtNumber.Text;
+            string powerText = txtPower.Text;
+            if (string.IsNullOrWhiteSpace(numberText) || string.IsNullOrWhiteSpace(powerText)) //boş olup olmadığını kontrol ediyor
+            {
+                MessageBox.Show("Please enter a number and exponent value.");
+                return;
+            }
+
+            if (!double.TryParse(numberText, out double sayi) || !int.TryParse(powerText, out int kuvvet))  //use double ifade girersek bu dönecek
+            {
+                MessageBox.Show("Please enter valid numeric values.");
+                return;
+            }
+            /*
+          
+              Math.Pow(number,power)
+            */
+
+            double result = Math.Pow(sayi, kuvvet);
+            txtResult.Text = $"{result}";
+        }
+    }
+}
