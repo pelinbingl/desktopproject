7efb392a723e7deb50d7b417a6aaed1910be0984
Pelin BİNGÖL <126492207+pelinbingl@users.noreply.github.com>
2023-08-05 00:01:49 +0300

    Add files via upload

---
+++ MonthOfTheYearCorrespondingToTheNumber/MonthOfTheYearCorrespondingToTheNumber/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace MonthOfTheYearCorrespondingToTheNumber
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFind = new System.Windows.Forms.Button();
+            this.labal1 = new System.Windows.Forms.Label();
+            this.lblNumber = new System.Windows.Forms.Label();
+            this.lblMonth = new System.Windows.Forms.Label();
+            this.txtMonth = new System.Windows.Forms.TextBox();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnFind
+            // 
+            this.btnFind.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnFind.Location = new System.Drawing.Point(261, 251);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(92, 53);
+            this.btnFind.TabIndex = 0;
+            this.btnFind.Text = "FIND";
+            this.btnFind.UseVisualStyleBackColor = true;
+            this.btnFind.Click += new System.EventHandler(this.btnFind_Click);
+            // 
+            // labal1
+            // 
+            this.labal1.AutoSize = true;
+            this.labal1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labal1.Location = new System.Drawing.Point(62, 49);
+            this.labal1.Name = "labal1";
+            this.labal1.Size = new System.Drawing.Size(268, 32);
+            this.labal1.TabIndex = 1;
+            this.labal1.Text = "Finding The Month";
+            // 
+            // lblNumber
+            // 
+            this.lblNumber.AutoSize = true;
+            this.lblNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblNumber.Location = new System.Drawing.Point(65, 120);
+            this.lblNumber.Name = "lblNumber";
+            this.lblNumber.Size = new System.Drawing.Size(85, 22);
+            this.lblNumber.TabIndex = 2;
+            this.lblNumber.Text = "Number:";
+            // 
+            // lblMonth
+            // 
+            this.lblMonth.AutoSize = true;
+            this.lblMonth.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblMonth.Location = new System.Drawing.Point(65, 184);
+            this.lblMonth.Name = "lblMonth";
+            this.lblMonth.Size = new System.Drawing.Size(70, 22);
+            this.lblMonth.TabIndex = 3;
+            this.lblMonth.Text = "Month:";
+            // 
+            // txtMonth
+            // 
+            this.txtMonth.Location = new System.Drawing.Point(176, 184);
+            this.txtMonth.Name = "txtMonth";
+            this.txtMonth.Size = new System.Drawing.Size(100, 22);
+            this.txtMonth.TabIndex = 4;
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Location = new System.Drawing.Point(176, 117);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(100, 22);
+            this.txtNumber.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(442, 440);
+            this.Controls.Add(this.txtNumber);
+            this.Controls.Add(this.txtMonth);
+            this.Controls.Add(this.lblMonth);
+            this.Controls.Add(this.lblNumber);
+            this.Controls.Add(this.labal1);
+            this.Controls.Add(this.btnFind);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFind;
+        private System.Windows.Forms.Label labal1;
+        private System.Windows.Forms.Label lblNumber;
+        private System.Windows.Forms.Label lblMonth;
+        private System.Windows.Forms.TextBox txtMonth;
+        private System.Windows.Forms.TextBox txtNumber;
+    }
+}
+
---
+++ MonthOfTheYearCorrespondingToTheNumber/MonthOfTheYearCorrespondingToTheNumber/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MonthOfTheYearCorrespondingToTheNumber
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+            string monthNumberText = txtNumber.Text;
+            if (!string.IsNullOrWhiteSpace(monthNumberText) && int.TryParse(monthNumberText, out int monthNumber))
+            {
+                if (monthNumber >= 1 && monthNumber <= 12)
+                {
+                    string[] months = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+                    string MonthName = months[monthNumber - 1];
+                    txtMonth.Text = $" {MonthName}";
+                }
+                else
+                {
+                    MessageBox.Show("Enter a valid month number (1-12).");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid number.");
+            }
+        }
+    }
+}
