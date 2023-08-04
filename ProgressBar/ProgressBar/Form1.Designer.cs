5f9be21d6c002e3f89c4456fa12d62ab5f5d357f
Pelin BİNGÖL <126492207+pelinbingl@users.noreply.github.com>
2023-08-05 00:10:28 +0300

    Add files via upload

---
+++ ProgressBar/ProgressBar/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace ProgressBar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressPercent = new System.Windows.Forms.ProgressBar();
+            this.btnMin = new System.Windows.Forms.Button();
+            this.btnMax = new System.Windows.Forms.Button();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.lblPercent = new System.Windows.Forms.Label();
+            this.txtPercent = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // progressPercent
+            // 
+            this.progressPercent.Location = new System.Drawing.Point(31, 87);
+            this.progressPercent.Name = "progressPercent";
+            this.progressPercent.Size = new System.Drawing.Size(441, 41);
+            this.progressPercent.TabIndex = 0;
+            // 
+            // btnMin
+            // 
+            this.btnMin.Location = new System.Drawing.Point(170, 153);
+            this.btnMin.Name = "btnMin";
+            this.btnMin.Size = new System.Drawing.Size(75, 23);
+            this.btnMin.TabIndex = 1;
+            this.btnMin.Text = "-";
+            this.btnMin.UseVisualStyleBackColor = true;
+            this.btnMin.Click += new System.EventHandler(this.btnMin_Click);
+            // 
+            // btnMax
+            // 
+            this.btnMax.Location = new System.Drawing.Point(170, 209);
+            this.btnMax.Name = "btnMax";
+            this.btnMax.Size = new System.Drawing.Size(75, 23);
+            this.btnMax.TabIndex = 2;
+            this.btnMax.Text = "+";
+            this.btnMax.UseVisualStyleBackColor = true;
+            this.btnMax.Click += new System.EventHandler(this.btnMax_Click);
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(143, 271);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(134, 23);
+            this.btnShow.TabIndex = 3;
+            this.btnShow.Text = "SHOW";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // lblPercent
+            // 
+            this.lblPercent.AutoSize = true;
+            this.lblPercent.Location = new System.Drawing.Point(191, 100);
+            this.lblPercent.Name = "lblPercent";
+            this.lblPercent.Size = new System.Drawing.Size(14, 16);
+            this.lblPercent.TabIndex = 4;
+            this.lblPercent.Text = "0";
+            // 
+            // txtPercent
+            // 
+            this.txtPercent.Location = new System.Drawing.Point(119, 41);
+            this.txtPercent.Name = "txtPercent";
+            this.txtPercent.Size = new System.Drawing.Size(185, 22);
+            this.txtPercent.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtPercent);
+            this.Controls.Add(this.lblPercent);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.btnMax);
+            this.Controls.Add(this.btnMin);
+            this.Controls.Add(this.progressPercent);
+            this.Name = "Form1";
+            this.Text = "Goster";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressPercent;
+        private System.Windows.Forms.Button btnMin;
+        private System.Windows.Forms.Button btnMax;
+        private System.Windows.Forms.Button btnShow;
+        private System.Windows.Forms.Label lblPercent;
+        private System.Windows.Forms.TextBox txtPercent;
+    }
+}
+
---
+++ ProgressBar/ProgressBar/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProgressBar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            progressPercent.Value = 0;
+            UpdateText();
+        }
+        private void btnMin_Click(object sender, EventArgs e)
+        {
+            progressPercent.Value = Math.Max(progressPercent.Value - 10, progressPercent.Minimum);
+            UpdateText();
+        }
+
+        private void btnMax_Click(object sender, EventArgs e)
+        {
+            progressPercent.Value = Math.Min(progressPercent.Value + 10, progressPercent.Maximum);
+            UpdateText();
+        }
+        private void UpdateText()
+        {
+            int percentage = (progressPercent.Value - progressPercent.Minimum) * 100 / (progressPercent.Maximum - progressPercent.Minimum);
+            txtPercent.Text = $"%{percentage}";
+            lblPercent.Text = $"%{percentage}";
+        }
+
+
+    }
+}
